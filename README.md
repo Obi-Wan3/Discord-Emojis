2b8f03985163589bcdad1179dca3ee7a8e7b3159
Obi-Wan3 <44986166+Obi-Wan3@users.noreply.github.com>
2021-04-18 11:25:47 -0700

    finish script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Discord-Emojis
+
+Flatten the list of Discord Emojis. The `emojis.json` file should be replaced with:
+- inspect element 
+- go to the "sources" tab
+- find `assets/bd028ce6ab8036567bcb.js`
+- 32nd search result of `function(e){e.exports=JSON.parse` (starts similarly with provided `emojis.json`)
+- save that JSON as `emojis.json` in this directory
---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import json
+
+if not os.path.isfile("./emojis.json"):
+    print("No emojis.json file found!")
+    sys.exit()
+
+with open("./emojis.json", "r") as f:
+    data = json.load(f)
+
+all_emojis = []
+
+# Loop through main categories
+for category in data.values():
+    # Loop through each category
+    for emoji in category:
+        all_emojis.append(emoji["surrogates"])
+
+print(all_emojis)
